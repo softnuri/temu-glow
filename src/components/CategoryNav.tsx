c224581895442a47294c8b32c28fcb8abaa65813
Jin <softnurijin@gmail.com>
2025-01-03 15:27:51 +0700

    - Navbar와 CategoryNav 의 스크롤시 반응형으로 조정 및 수정

--- src/components/CategoryNav.tsx
+++ src/components/CategoryNav.tsx
@@ -16,13 +16,37 @@ const categories = [
 const CategoryNav = () => {
     return (
         <div className="bg-white shadow-sm mb-6 sticky top-[120px] md:top-[64px] z-10">
-            <div className="container mx-auto px-4">
+            <div className="container mx-auto px-4 relative">
+                <style>
+                    {`
+                        .category-swiper .swiper-button-prev,
+                        .category-swiper .swiper-button-next {
+                            width: 30px;
+                            height: 30px;
+                            background-color: white;
+                            border-radius: 50%;
+                            box-shadow: 0 2px 4px rgba(0,0,0,1);
+                        }
+                        .category-swiper .swiper-button-prev {
+                            left: 0px;
+                        }
+                        .category-swiper .swiper-button-next {
+                            right: 0px;
+                        }
+                        .category-swiper .swiper-button-prev::after,
+                        .category-swiper .swiper-button-next::after {
+                            font-size: 20px;
+                            color:rgb(17, 96, 145);
+                            font-weight: bold;
+                        }
+                    `}
+                </style>
                 <Swiper
                     modules={[Navigation]}
                     spaceBetween={10}
                     slidesPerView="auto"
                     navigation
-                    className="py-4"
+                    className="category-swiper py-4"
                 >
                     {categories.map((category) => (
                         <SwiperSlide key={category.id} className="!w-auto">
